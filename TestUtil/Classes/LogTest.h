8abd54695707200214d5f53e61199bba619e2b6f
dorayo <yomingyo@gmail.com>
2019-01-08 23:10:51 +0800

    init
    Signed-off-by: dorayo <yomingyo@gmail.com>

---
+++ TestUtil/Classes/LogTest.h
@@ -0,0 +1,15 @@
+//
+//  LogTest.h
+//  TestDemo000
+//
+//  Created by 小飞鸟 on 2019/01/08.
+//  Copyright © 2019 小飞鸟. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LogTest : NSObject
+
++(void)logTest;
+
+@end
---
+++ TestUtil/Classes/LogTest.m
@@ -0,0 +1,16 @@
+//
+//  LogTest.m
+//  TestDemo000
+//
+//  Created by 小飞鸟 on 2019/01/08.
+//  Copyright © 2019 小飞鸟. All rights reserved.
+//
+
+#import "LogTest.h"
+
+@implementation LogTest
+
++(void)logTest{
+    NSLog(@"%s",__func__);
+}
+@end
